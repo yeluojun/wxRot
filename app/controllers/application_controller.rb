1779254f4381c0af416cddf1fb329cb8dbd21085
yeluojun <635143970@qq.com>
2016-06-23 18:41:29 +0800

    add login controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  attr_accessor :current_user
 end
