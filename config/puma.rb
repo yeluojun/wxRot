e1181b53a0e9c2fe3b9885c4310ae27bfe13cf7f
yeluojun <635143970@qq.com>
2018-05-09 21:35:20 +0800

    fixbug

--- config/puma.rb
+++ config/puma.rb
@@ -2,14 +2,14 @@
 
 require 'etc'
 
-env = ENV.fetch("RAILS_ENV") { "staging" }
+env = ENV.fetch("RAILS_ENV") { "production" }
 environment env
 
 deploy_to = File.expand_path('../' * 3, __FILE__)
 directory "#{deploy_to}/current"
 rackup "#{deploy_to}/current/config.ru"
 pidfile "#{deploy_to}/shared/tmp/pids/puma.pid"
-state_path "#{deploy_to}/shared/tmp/sockets/puma.state"
+state_path "#{deploy_to}/shared/tmp/sockets/puma.state"ß
 stdout_redirect "#{deploy_to}/shared/log/puma_access.log", "#{deploy_to}/shared/log/puma_error.log", true
 bind "unix://#{deploy_to}/shared/tmp/sockets/puma.sock"
 activate_control_app "unix://#{deploy_to}/shared/tmp/sockets/pumactl.sock"
