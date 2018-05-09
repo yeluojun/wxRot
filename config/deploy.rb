8fcb73b542ac0d89f69a878aab2ec8a1429c5efe
yeluojun <635143970@qq.com>
2018-05-09 21:28:26 +0800

    fixbug

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,11 +5,6 @@ require 'mina_sidekiq/tasks'
 require 'mina/rvm'    # for rvm support. (https://rvm.io)
 require 'mina/puma'
 
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
 set :domain, 'root@yeluojun.club'
 set :deploy_to, '/deploy/rails/wxRot'
 set :repository, 'git@github.com:yeluojun/wxRot.git'
@@ -25,21 +20,11 @@ set :rvm_use_path, "/usr/local/rvm/scripts/rvm"
 #   set :port, '30000'           # SSH port number.
 #   set :forward_agent, true     # SSH forward_agent.
 
-# Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
-# Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
-# run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
-# set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
 set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
 
-# This task is the environment that is loaded for all remote run commands, such as
-# `mina deploy` or `mina rake`.
+set :puma_config, -> { "#{fetch(:current_path)}/config/mina.puma.rb" }
+
 task :remote_environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
-  # invoke :'rbenv:load'
-
-  # For those using RVM, use this to load an RVM version@gemset.
-
   invoke :'rvm:use', 'ruby-2.4.1'
 end
 
